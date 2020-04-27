2f2e39923f2310f39eba5eb34028af8bae4d3ed3
Fabien Parent <fparent@baylibre.com>
2020-04-27 14:41:21 +0200

    mt8183: add secure boot support
    Add secure boot's boilerplate code.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- plat/mediatek/mt8183/platform.mk
+++ plat/mediatek/mt8183/platform.mk
@@ -115,3 +115,35 @@ $(eval $(call add_define,MACH_MT8183))
 
 include lib/coreboot/coreboot.mk
 
+ifneq (${TRUSTED_BOARD_BOOT},0)
+
+include drivers/auth/mbedtls/mbedtls_crypto.mk
+include drivers/auth/mbedtls/mbedtls_x509.mk
+
+AUTH_SOURCES		:=	drivers/auth/auth_mod.c			\
+				drivers/auth/crypto_mod.c		\
+				drivers/auth/img_parser_mod.c		\
+				drivers/auth/tbbr/tbbr_cot.c
+
+BL2_SOURCES		+=	${AUTH_SOURCES}				\
+				plat/common/tbbr/plat_tbbr.c	\
+				${MTK_PLAT_SOC}/mtk_tbbr.c	\
+				${MTK_PLAT_SOC}/mtk_rotpk.S
+
+ROT_KEY		=	$(BUILD_PLAT)/rot_key.pem
+ROTPK_HASH		=	$(BUILD_PLAT)/rotpk_sha256.bin
+
+$(eval $(call add_define_val,ROTPK_HASH,'"$(ROTPK_HASH)"'))
+$(BUILD_PLAT)/bl1/mtk_rotpk.o: $(ROTPK_HASH)
+$(BUILD_PLAT)/bl2/mtk_rotpk.o: $(ROTPK_HASH)
+
+certificates: $(ROT_KEY)
+$(ROT_KEY): | $(BUILD_PLAT)
+	@echo "  OPENSSL $@"
+	$(Q)openssl genrsa 2048 > $@ 2>/dev/null
+
+$(ROTPK_HASH): $(ROT_KEY)
+	@echo "  OPENSSL $@"
+	$(Q)openssl rsa -in $< -pubout -outform DER 2>/dev/null |\
+	openssl dgst -sha256 -binary > $@ 2>/dev/null
+endif
