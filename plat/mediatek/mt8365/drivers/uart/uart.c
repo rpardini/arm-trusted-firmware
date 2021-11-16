1fa6d6c4bd140f968cd9abb72262d3866644226d
Alexandre Bailon <abailon@baylibre.com>
2021-11-16 10:27:02 +0100

    mt8365: Update uart driver
    This updates the uart driver with some changes required
    for power management (suspend / resume).
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

--- plat/mediatek/mt8365/drivers/uart/uart.c
+++ plat/mediatek/mt8365/drivers/uart/uart.c
@@ -1,33 +1,29 @@
-/*
- * Copyright (c) 2019, ARM Limited and Contributors. All rights reserved.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include <lib/mmio.h>
+#include <mmio.h>
 #include <uart.h>
 
-static struct mt_uart uart_save_addr[DRV_SUPPORT_UART_PORTS];
+struct mtk_uart UART_SaveAddr[DRV_SUPPORT_UART_PORTS];
 
-static const unsigned int uart_base_addr[DRV_SUPPORT_UART_PORTS] = {
-	UART0_BASE,
-	UART1_BASE
+const unsigned int UART_BaseAddr[DRV_SUPPORT_UART_PORTS] = {
+	0x11002000,
+	0x11003000,
+	0,
+	0,
+	0,
+	0,
 };
 
-void mt_uart_restore(void)
+void mtk_uart_restore(void)
 {
-	int uart_idx = UART_PORT0;
-	struct mt_uart *uart;
 	unsigned long base;
+	int uart_idx = UART_PORT0;
+	struct mtk_uart *uart;
 
-	/* Must NOT print any debug log before UART restore */
-	for (uart_idx = UART_PORT0; uart_idx < HW_SUPPORT_UART_PORTS;
-	     uart_idx++) {
-
-		uart = &uart_save_addr[uart_idx];
+	/* Attention!! Must NOT print any debug log before UART restore */
+	for (uart_idx = UART_PORT0; uart_idx < HW_SUPPORT_UART_PORTS; uart_idx++) {
+		uart = &UART_SaveAddr[uart_idx];
 		base = uart->base;
 
-		mmio_write_32(UART_LCR(base), UART_LCR_MODE_B);
+		mmio_write_32(UART_LCR(base), 0xbf);
 		mmio_write_32(UART_EFR(base), uart->registers.efr);
 		mmio_write_32(UART_LCR(base), uart->registers.lcr);
 		mmio_write_32(UART_FCR(base), uart->registers.fcr);
@@ -36,40 +32,34 @@ void mt_uart_restore(void)
 		mmio_write_32(UART_HIGHSPEED(base), uart->registers.highspeed);
 		mmio_write_32(UART_FRACDIV_L(base), uart->registers.fracdiv_l);
 		mmio_write_32(UART_FRACDIV_M(base), uart->registers.fracdiv_m);
-		mmio_write_32(UART_LCR(base),
-			      uart->registers.lcr | UART_LCR_DLAB);
+		mmio_write_32(UART_LCR(base), uart->registers.lcr | UART_LCR_DLAB);
 		mmio_write_32(UART_DLL(base), uart->registers.dll);
 		mmio_write_32(UART_DLH(base), uart->registers.dlh);
 		mmio_write_32(UART_LCR(base), uart->registers.lcr);
-		mmio_write_32(UART_SAMPLE_COUNT(base),
-			      uart->registers.sample_count);
-		mmio_write_32(UART_SAMPLE_POINT(base),
-			      uart->registers.sample_point);
+		mmio_write_32(UART_SAMPLE_COUNT(base), uart->registers.sample_count);
+		mmio_write_32(UART_SAMPLE_POINT(base), uart->registers.sample_point);
 		mmio_write_32(UART_GUARD(base), uart->registers.guard);
 
 		/* flow control */
 		mmio_write_32(UART_ESCAPE_EN(base), uart->registers.escape_en);
 		mmio_write_32(UART_MCR(base), uart->registers.mcr);
 		mmio_write_32(UART_IER(base), uart->registers.ier);
-		mmio_write_32(UART_SCR(base), uart->registers.scr);
 	}
 }
 
-void mt_uart_save(void)
+void mtk_uart_save(void)
 {
 	int uart_idx = UART_PORT0;
-	struct mt_uart *uart;
 	unsigned long base;
+	struct mtk_uart *uart;
 
-	for (uart_idx = UART_PORT0; uart_idx < HW_SUPPORT_UART_PORTS;
-	     uart_idx++) {
-
-		uart_save_addr[uart_idx].base = uart_base_addr[uart_idx];
-		base = uart_base_addr[uart_idx];
-		uart = &uart_save_addr[uart_idx];
+	for (uart_idx = UART_PORT0; uart_idx < HW_SUPPORT_UART_PORTS; uart_idx++) {
+		UART_SaveAddr[uart_idx].base = UART_BaseAddr[uart_idx];
+		base = UART_BaseAddr[uart_idx];
+		uart = &UART_SaveAddr[uart_idx];
 		uart->registers.lcr = mmio_read_32(UART_LCR(base));
 
-		mmio_write_32(UART_LCR(base), UART_LCR_MODE_B);
+		mmio_write_32(UART_LCR(base), 0xbf);
 		uart->registers.efr = mmio_read_32(UART_EFR(base));
 		mmio_write_32(UART_LCR(base), uart->registers.lcr);
 		uart->registers.fcr = mmio_read_32(UART_FCR_RD(base));
@@ -78,34 +68,17 @@ void mt_uart_save(void)
 		uart->registers.highspeed = mmio_read_32(UART_HIGHSPEED(base));
 		uart->registers.fracdiv_l = mmio_read_32(UART_FRACDIV_L(base));
 		uart->registers.fracdiv_m = mmio_read_32(UART_FRACDIV_M(base));
-		mmio_write_32(UART_LCR(base),
-			      uart->registers.lcr | UART_LCR_DLAB);
+		mmio_write_32(UART_LCR(base), uart->registers.lcr | UART_LCR_DLAB);
 		uart->registers.dll = mmio_read_32(UART_DLL(base));
 		uart->registers.dlh = mmio_read_32(UART_DLH(base));
 		mmio_write_32(UART_LCR(base), uart->registers.lcr);
-		uart->registers.sample_count = mmio_read_32(
-						UART_SAMPLE_COUNT(base));
-		uart->registers.sample_point = mmio_read_32(
-						UART_SAMPLE_POINT(base));
+		uart->registers.sample_count = mmio_read_32(UART_SAMPLE_COUNT(base));
+		uart->registers.sample_point = mmio_read_32(UART_SAMPLE_POINT(base));
 		uart->registers.guard = mmio_read_32(UART_GUARD(base));
 
 		/* flow control */
 		uart->registers.escape_en = mmio_read_32(UART_ESCAPE_EN(base));
 		uart->registers.mcr = mmio_read_32(UART_MCR(base));
 		uart->registers.ier = mmio_read_32(UART_IER(base));
-		uart->registers.scr = mmio_read_32(UART_SCR(base));
 	}
-}
-
-void mt_console_uart_cg(int on)
-{
-	if (on)
-		mmio_write_32(UART_CLOCK_GATE_CLR, UART0_CLOCK_GATE_BIT);
-	else
-		mmio_write_32(UART_CLOCK_GATE_SET, UART0_CLOCK_GATE_BIT);
-}
-
-int mt_console_uart_cg_status(void)
-{
-	return mmio_read_32(UART_CLOCK_GATE_STA) & UART0_CLOCK_GATE_BIT;
 }
