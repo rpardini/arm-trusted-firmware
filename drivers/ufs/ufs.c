fc5f9a74984eba616bac21c0d3be2810e0e150fd
Fabien Parent <fparent@baylibre.com>
2021-11-30 15:40:13 +0100

    ufs: wait until DEVICE_INIT flag is cleared by device
    The UFS spec says that the host must wait for the DEVICE_INIT flag
    to be cleared before continuing operation. Do that by probing the
    flag.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- drivers/ufs/ufs.c
+++ drivers/ufs/ufs.c
@@ -752,6 +752,7 @@ static void ufs_enum(void)
 {
 	unsigned int blk_num, blk_size;
 	int i;
+	int flag = FLAG_DEVICE_INIT;
 
 	/* 0 means 1 slot */
 	nutrs = (mmio_read_32(ufs_params.reg_base + CAP) & CAP_NUTRS_MASK) + 1;
@@ -762,7 +763,11 @@ static void ufs_enum(void)
 	ufs_verify_ready();
 
 	ufs_set_flag(FLAG_DEVICE_INIT);
-	mdelay(200);
+
+	/* wait for the DEVICE_INIT flag to be reset by the device */
+	for (i = 0; i < 1000 && flag == FLAG_DEVICE_INIT; i++)
+		flag = ufs_read_flag(FLAG_DEVICE_INIT);
+
 	/* dump available LUNs */
 	for (i = 0; i < UFS_MAX_LUNS; i++) {
 		ufs_read_capacity(i, &blk_num, &blk_size);
