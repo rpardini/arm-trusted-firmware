23f07c08e18938a7b23074b77a8ed63e0f55b6e2
Adrien Ricciardi <aricciardi@baylibre.com>
2022-03-15 15:38:48 +0100

    mediatek: mt8365: fixed build
    Signed-off-by: Adrien Ricciardi <aricciardi@baylibre.com>

--- plat/mediatek/mt8365/platform.mk
+++ plat/mediatek/mt8365/platform.mk
@@ -105,6 +105,7 @@ BL31_SOURCES    += common/desc_image_load.c                              \
                    ${MTK_PLAT_SOC}/plat_dfd.c                            \
                    ${MTK_PLAT_SOC}/plat_sip_svc.c                        \
                    ${MTK_PLAT}/common/mtk_sip_svc.c                      \
+                   ${MTK_PLAT}/common/mtk_hwid.c                         \
                    ${MTK_PLAT_SOC}/scu.c
 
 BL31_LIBS += ${LIBDRAM} ${LIBBASE}
