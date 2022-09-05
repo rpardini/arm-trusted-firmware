af95ff4aba95ff206aeff45ff7e4aadda88143f4
Dawei Chien <dawei.chien@mediatek.com>
2022-09-05 19:08:59 +0800

    UPSTREAM: feat(mt8188): add EMI MPU basic drivers
    EMI MPU stands for external memory interface memory protect unit.
    MT8188 supports 32 regions and 16 domains.
    
    Signed-off-by: Dawei Chien <dawei.chien@mediatek.com>
    Change-Id: I9bbeb355665401cc71dda6db22157d9d751570d1
    cherry picked from commit 8454f0d65eeb85b72f454376faa0f7a15226e240
    Signed-off-by: Chris-QJ Chen <chris-qj.chen@mediatek.com>

--- plat/mediatek/mt8188/platform.mk
+++ plat/mediatek/mt8188/platform.mk
@@ -29,6 +29,7 @@ MODULES-y += $(MTK_PLAT)/drivers/cpu_pm
 MODULES-y += $(MTK_PLAT)/drivers/dcm
 MODULES-y += $(MTK_PLAT)/drivers/dfd
 MODULES-y += $(MTK_PLAT)/drivers/dp
+MODULES-y += $(MTK_PLAT)/drivers/emi_mpu
 MODULES-y += $(MTK_PLAT)/drivers/gic600
 MODULES-y += $(MTK_PLAT)/drivers/gpio
 MODULES-y += $(MTK_PLAT)/drivers/iommu
