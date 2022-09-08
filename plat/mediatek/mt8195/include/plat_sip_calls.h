a56bf0ea98af19ba883704923dcd5b304cdc3bfc
Bo-Chen Chen <rex-bc.chen@mediatek.com>
2022-09-08 13:14:47 +0800

    UPSTREAM: refactor(mediatek): refactor plat_sip_calls.h for mt8192/mt8195/mt8186
    - MTK_SIP_KERNEL_DFD can be moved to mtk_sip_def.h.
    - Remove unused MTK_SIP_* definations which are already defined in
      mtk_sip_def.h.
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: Ife8f1e842d986691488548632426f194199d42b9
    cherry picked from commit 3bdd9a24e953b3b38acaf01eb4f173f62983da93
    Signed-off-by: Chris-QJ Chen <chris-qj.chen@mediatek.com>

--- plat/mediatek/mt8195/include/plat_sip_calls.h
+++ plat/mediatek/mt8195/include/plat_sip_calls.h
@@ -10,14 +10,6 @@
 /*******************************************************************************
  * Plat SiP function constants
  ******************************************************************************/
-#define MTK_PLAT_SIP_NUM_CALLS    4
-
-/* DFD */
-#define MTK_SIP_KERNEL_DFD_AARCH32	0x82000205
-#define MTK_SIP_KERNEL_DFD_AARCH64	0xC2000205
-
-/* DP/eDP */
-#define MTK_SIP_DP_CONTROL_AARCH32	0x82000523
-#define MTK_SIP_DP_CONTROL_AARCH64	0xC2000523
+#define MTK_PLAT_SIP_NUM_CALLS    (8)
 
 #endif /* PLAT_SIP_CALLS_H */
