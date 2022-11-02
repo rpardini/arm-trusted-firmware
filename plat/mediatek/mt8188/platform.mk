dc292eebeb6e8fd3bf9271db34c2684c6f32e3b3
Chris-QJ Chen <chris-qj.chen@mediatek.com>
2022-11-02 15:08:01 +0800

    GENIO: feat(mediatek): enable L2 cache reconfigure driver
    Enable mt8188 L2 cache reconfigure driver
    
    Change-Id: If1372219f73baf87c89b358a9b08b13b540163d2
    Signed-off-by: Chris-QJ Chen <chris-qj.chen@mediatek.com>

--- plat/mediatek/mt8188/platform.mk
+++ plat/mediatek/mt8188/platform.mk
@@ -45,6 +45,7 @@ MODULES-y += $(MTK_PLAT)/drivers/uart
 MODULES-y += $(MTK_PLAT)/helpers
 MODULES-y += $(MTK_PLAT)/topology
 ifeq (${MTK_IOT_YOCTO},1)
+MODULES-y += $(MTK_PLAT)/drivers/l2c
 MODULES-y += $(MTK_PLAT)/drivers/apusys
 MODULES-y += $(MTK_PLAT_SOC)/drivers/apusys
 MODULES-y += $(MTK_PLAT_SOC)/drivers/rng
