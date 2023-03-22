345d30607a0d818d5299930b17e2677472aa3a85
junxian.zeng <junxian.zeng@mediatek.com>
2023-03-22 15:40:41 +0800

    GENIO: mediatek: 8195: apusys: Add regdump
    add apusys regdump for apusys security boot
    
    Change-Id: If387ba961999d5cce38f2c304868b553ee022a77
    Signed-off-by: junxian.zeng <junxian.zeng@mediatek.com>

---
+++ plat/mediatek/mt8195/drivers/apusys/apusys_regdump.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <common/debug.h>
+#include <lib/mmio.h>
+#include <lib/utils_def.h>
+#include <mtk_mmap_pool.h>
+#include <platform_def.h>
+
+int apusys_regdump(uint64_t dst_pa, unsigned int buffer_size)
+{
+
+	return 0;
+}
