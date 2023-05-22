fd249f00c4408ea5a783d8622189b496397cb067
ht.lin <ht.lin@mediatek.com>
2023-05-22 15:10:06 +0800

    GENIO: mediatek: ufs: enable HS-G1 mode
    Implement mtk_ufs_phy_set_pwr_mode to enable ufs HS-G1 mode.
    
    Change-Id: Id07c2aac6d27915409515550d1b0e777fa1a620a
    Signed-off-by: ht.lin <ht.lin@mediatek.com>

--- plat/mediatek/common/drivers/ufs/mtk-ufs.c
+++ plat/mediatek/common/drivers/ufs/mtk-ufs.c
@@ -1,7 +1,10 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <drivers/ufs.h>
+#include <lib/mmio.h>
 #include <common/debug.h>
+
+#include "mtk-ufs.h"
 
 static int mtk_ufs_phy_init(ufs_params_t *params)
 {
@@ -10,6 +13,60 @@ static int mtk_ufs_phy_init(ufs_params_t *params)
 
 static int mtk_ufs_phy_set_pwr_mode(ufs_params_t *params)
 {
+	unsigned int data, tx_lanes, rx_lanes;
+	uintptr_t base;
+	unsigned int flags;
+
+	base = params->reg_base;
+	flags = params->flags;
+	if ((flags & UFS_FLAGS_VENDOR_SKHYNIX) != 0U) {
+		NOTICE("ufs: H**** device must set VS_DebugSaveConfigTime 0x10\n");
+		/* VS_DebugSaveConfigTime */
+		ufshc_dme_set(0xd0a0, 0x0, 0x10);
+		/* sync length */
+		ufshc_dme_set(0x1556, 0x0, 0x48);
+	}
+
+	ufshc_dme_get(PA_TACTIVATE_OFFSET, 0, &data);
+	if (data < 7)
+		ufshc_dme_set(PA_TACTIVATE_OFFSET, 0, 7);
+
+	ufshc_dme_get(PA_CONNECTED_TX_DATA_LANES_OFFSET, 0, &tx_lanes);
+	ufshc_dme_get(PA_CONNECTED_RX_DATA_LANES_OFFSET, 0, &rx_lanes);
+
+	ufshc_dme_set(PA_TX_SKIP_OFFSET, 0, 0);
+	ufshc_dme_set(PA_TX_GEAR_OFFSET, 0, 3);
+	ufshc_dme_set(PA_RX_GEAR_OFFSET, 0, 3);
+	ufshc_dme_set(PA_HS_SERIES_OFFSET, 0, 2);
+	ufshc_dme_set(PA_TX_TERMINATION_OFFSET, 0, 1);
+	ufshc_dme_set(PA_RX_TERMINATION_OFFSET, 0, 1);
+	ufshc_dme_set(PA_SCRAMBLING_OFFSET, 0, 0);
+	ufshc_dme_set(PA_ACTIVE_TX_DATA_LANES_OFFSET, 0, tx_lanes);
+	ufshc_dme_set(PA_ACTIVE_RX_DATA_LANES_OFFSET, 0, rx_lanes);
+	ufshc_dme_set(PA_PWR_MODE_USER_DATA0_OFFSET, 0, 8191);
+	ufshc_dme_set(PA_PWR_MODE_USER_DATA1_OFFSET, 0, 65535);
+	ufshc_dme_set(PA_PWR_MODE_USER_DATA2_OFFSET, 0, 32767);
+	ufshc_dme_set(DME_FC0_PROTECTION_TIMEOUT_OFFSET, 0, 8191);
+	ufshc_dme_set(DME_TC0_REPLAY_TIMEOUT_OFFSET, 0, 65535);
+	ufshc_dme_set(DME_AFC0_REQ_TIMEOUT_OFFSET, 0, 32767);
+	ufshc_dme_set(PA_PWR_MODE_USER_DATA3_OFFSET, 0, 8191);
+	ufshc_dme_set(PA_PWR_MODE_USER_DATA4_OFFSET, 0, 65535);
+	ufshc_dme_set(PA_PWR_MODE_USER_DATA5_OFFSET, 0, 32767);
+	ufshc_dme_set(DME_FC1_PROTECTION_TIMEOUT_OFFSET, 0, 8191);
+	ufshc_dme_set(DME_TC1_REPLAY_TIMEOUT_OFFSET, 0, 65535);
+	ufshc_dme_set(DME_AFC1_REQ_TIMEOUT_OFFSET, 0, 32767);
+
+	ufshc_dme_set(PA_PWR_MODE_OFFSET, 0, 0x11);
+	do {
+		data = mmio_read_32(base + IS);
+	} while ((data & UFS_INT_UPMS) == 0);
+	mmio_write_32(base + IS, UFS_INT_UPMS);
+	data = mmio_read_32(base + HCS);
+	if ((data & HCS_UPMCRS_MASK) == HCS_PWR_LOCAL)
+		INFO("ufs: change power mode success\n");
+	else
+		WARN("ufs: HCS.UPMCRS error, HCS:0x%x\n", data);
+
 	return 0;
 }
 
--- plat/mediatek/common/drivers/ufs/mtk-ufs.h
+++ plat/mediatek/common/drivers/ufs/mtk-ufs.h
@@ -2,6 +2,52 @@
 
 #include <drivers/ufs.h>
 
+#define RX_MIN_ACTIVATETIME_CAP_OFFSET          0x008F
+#define RX_HS_G2_SYNC_LENGTH_CAP_OFFSET         0x0094
+#define RX_HS_G3_SYNC_LENGTH_CAP_OFFSET         0x0095
+
+#define PA_AVAIL_TX_DATA_LANES_OFFSET           0x1520
+#define PA_TX_SKIP_OFFSET                       0x155C
+#define PA_TX_SKIP_PERIOD_OFFSET                0x155D
+#define PA_LOCAL_TX_LCC_ENABLE_OFFSET           0x155E
+#define PA_ACTIVE_TX_DATA_LANES_OFFSET          0x1560
+#define PA_CONNECTED_TX_DATA_LANES_OFFSET       0x1561
+#define PA_TX_TRAILING_CLOCKS_OFFSET            0x1564
+#define PA_TX_GEAR_OFFSET                       0x1568
+#define PA_TX_TERMINATION_OFFSET                0x1569
+#define PA_HS_SERIES_OFFSET                     0x156A
+#define PA_PWR_MODE_OFFSET                      0x1571
+#define PA_ACTIVE_RX_DATA_LANES_OFFSET          0x1580
+#define PA_CONNECTED_RX_DATA_LANES_OFFSET       0x1581
+#define PA_RX_PWR_STATUS_OFFSET                 0x1582
+#define PA_RX_GEAR_OFFSET                       0x1583
+#define PA_RX_TERMINATION_OFFSET                0x1584
+#define PA_SCRAMBLING_OFFSET                    0x1585
+#define PA_MAX_RX_PWM_GEAR_OFFSET               0x1586
+#define PA_MAX_RX_HS_GEAR_OFFSET                0x1587
+#define PA_PACP_REQ_TIMEOUT_OFFSET              0x1590
+#define PA_PACP_REQ_EOB_TIMEOUT_OFFSET          0x1591
+#define PA_REMOTE_VER_INFO_OFFSET               0x15A0
+#define PA_LOGICAL_LANE_MAP_OFFSET              0x15A1
+#define PA_TACTIVATE_OFFSET                     0x15A8
+#define PA_PWR_MODE_USER_DATA0_OFFSET           0x15B0
+#define PA_PWR_MODE_USER_DATA1_OFFSET           0x15B1
+#define PA_PWR_MODE_USER_DATA2_OFFSET           0x15B2
+#define PA_PWR_MODE_USER_DATA3_OFFSET           0x15B3
+#define PA_PWR_MODE_USER_DATA4_OFFSET           0x15B4
+#define PA_PWR_MODE_USER_DATA5_OFFSET           0x15B5
+
+#define DL_TC0_TX_FC_THRESHOLD_OFFSET           0x2040
+#define DL_AFC0_CREDIT_THRESHOLD_OFFSET         0x2044
+#define DL_TC0_OUT_ACK_THRESHOLD_OFFSET         0x2045
+
+#define DME_FC0_PROTECTION_TIMEOUT_OFFSET       0xD041
+#define DME_TC0_REPLAY_TIMEOUT_OFFSET           0xD042
+#define DME_AFC0_REQ_TIMEOUT_OFFSET             0xD043
+#define DME_FC1_PROTECTION_TIMEOUT_OFFSET       0xD044
+#define DME_TC1_REPLAY_TIMEOUT_OFFSET           0xD045
+#define DME_AFC1_REQ_TIMEOUT_OFFSET             0xD046
+
 void mtk_ufs_init(ufs_params_t *params);
 
 #endif
