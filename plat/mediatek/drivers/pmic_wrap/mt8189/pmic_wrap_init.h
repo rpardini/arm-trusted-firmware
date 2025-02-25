dd67b14459960bb060fe6a0b24a636c43ce88aad
Zhigang Qin <zhigang.qin@mediatek.corp-partner.google.com>
2025-02-25 21:59:07 +0800

    UPSTREAM: BACKPORT: feat(mt8189): add PWRAP driver
    Add PWRAP driver for mt6365 pmic communication
    
    Signed-off-by: Zhigang Qin <zhigang.qin@mediatek.corp-partner.google.com>
    Change-Id: I4d142cb020f0e1b0372f7ef2ba1eb03287cbcd8f

--- plat/mediatek/drivers/pmic_wrap/mt8189/pmic_wrap_init.h
+++ plat/mediatek/drivers/pmic_wrap/mt8189/pmic_wrap_init.h
@@ -11,19 +11,25 @@
 #include "platform_def.h"
 #include <pmic_wrap_init_common.h>
 
+#define PWRAP_DEBUG				0
+#define PMIF_SPI_SWINF_NO			2
+
 static struct mt8189_pmic_wrap_regs *const mtk_pwrap = (void *)PMIC_WRAP_BASE;
 
 /* PMIC_WRAP registers */
 struct mt8189_pmic_wrap_regs {
 	uint32_t init_done;
-	uint32_t reserved[543];
-	uint32_t wacs2_cmd;
-	uint32_t wacs2_wdata;
-	uint32_t reserved1[3];
-	uint32_t wacs2_rdata;
-	uint32_t reserved2[3];
-	uint32_t wacs2_vldclr;
-	uint32_t wacs2_sta;
+	uint32_t reserved[511];
+	struct {
+		uint32_t cmd;
+		uint32_t wdata;
+		uint32_t reserved1[3];
+		uint32_t rdata;
+		uint32_t reserved2[3];
+		uint32_t vldclr;
+		uint32_t sta;
+		uint32_t reserved3[5];
+	} wacs[4];
 };
 
 #endif /* PMIC_WRAP_INIT_H */
