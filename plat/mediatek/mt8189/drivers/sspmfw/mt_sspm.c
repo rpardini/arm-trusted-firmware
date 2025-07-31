fbc7b2b3e433abb9b2a62509a2608175ab00655b
hailong.fan <hailong.fan@mediatek.com>
2025-07-31 15:35:14 +0800

    GENIO: feat(mt8189): sspm: update sspm boot sequence
    Add SSPM SRAM power on setting.
    
    Change-Id: I5bb6328d98e6d7e99aeb1f9e5c8f140cb70f1c30
    Signed-off-by: hailong.fan <hailong.fan@mediatek.com>

--- plat/mediatek/mt8189/drivers/sspmfw/mt_sspm.c
+++ plat/mediatek/mt8189/drivers/sspmfw/mt_sspm.c
@@ -11,12 +11,13 @@
 #include <mcu.h>
 #include <lib/mtk_init/mtk_init.h>
 #include "mt_sspm.h"
-
-#define SSPM_SW_RSTN		(SSPM_CFG_BASE + 0x000)
+#include "mt_spm.h"
+#include "mt_spm_reg.h"
 
 static void reset_sspm(struct mtk_mcu *mcu)
 {
-	mmio_write_32(SSPM_SW_RSTN, 0x1);
+	mmio_write_32(SSPM_CFGREG_SYS_SEL, NON_CACHE_DIS | PURE_TCM_EN);
+	mmio_write_32(SSPM_SW_RSTN, SW_RSTN);
 }
 
 static struct mtk_mcu sspm = {
@@ -26,13 +27,30 @@ static struct mtk_mcu sspm = {
 	.reset = reset_sspm,
 };
 
+void sspm_enable_sram(void)
+{
+	mmio_write_32(SPM_BASE, SPM_PRJ_CODE);
+	mmio_write_32(SSPM_SRAM_CON, SSPM_SRAM_SLEEP_B | SSPM_SRAM_ISOINT_B);
+}
+
 int sspm_init(void)
 {
 	NOTICE("%s\n", __func__);
 
+	/* Set to secure on accessing SSPM TCM */
+	mmio_write_32(VLP_A0_APC_CON, 0);
+	mmio_write_32(VLP_A0_MAS_SEC_0, M2_SEC);
+
+	sspm_enable_sram();
+
+	mmio_write_32(SSPM_CK_EN, SSPM_CLK_EN_ALL);
+
+	mdelay(200);
+
 	sspm.run_size = sspm_bin_len;
 	if (mtk_init_mcu(&sspm))
 		ERROR("%s() failed\n", __func__);
+
 	return 0;
 }
 
--- plat/mediatek/mt8189/drivers/sspmfw/mt_sspm.h
+++ plat/mediatek/mt8189/drivers/sspmfw/mt_sspm.h
@@ -3,6 +3,24 @@
  * Copyright (C) 2025 MediaTek Inc.
  */
 
+#define SPM_PRJ_CODE		0xB160001
+#define SSPM_SRAM_SLEEP_B	0x10
+#define SSPM_SRAM_ISOINT_B	0x2
+
+#define SSPM_SW_RSTN		(SSPM_CFG_BASE + 0x000)
+#define SW_RSTN			BIT(0)
+
+#define SSPM_CFGREG_SYS_SEL	(SSPM_CFG_BASE + 0x160)
+#define NON_CACHE_DIS		BIT(1)
+#define PURE_TCM_EN		BIT(0)
+
+#define SSPM_CK_EN		(SSPM_CFG_BASE + 0x3000)
+#define SSPM_CLK_EN_ALL		0xFFFFFFFF
+
+#define VLP_A0_MAS_SEC_0	(DEVAPC_VLP_AO_BASE + 0xA00)
+#define M2_SEC			BIT(2)
+#define VLP_A0_APC_CON		(DEVAPC_VLP_AO_BASE + 0xF00)
+
 _Alignas(8) extern const unsigned char sspm_bin[];
 extern const unsigned char sspm_bin_end[];
 #define sspm_bin_len (sspm_bin_end - sspm_bin)
