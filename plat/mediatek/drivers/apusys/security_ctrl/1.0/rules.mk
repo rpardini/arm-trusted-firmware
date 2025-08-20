9761f39259b40f55ff11e56ac1c63a8caaa66abc
Chungying Lu <chungying.lu@mediatek.com>
2025-08-20 17:39:25 +0800

    GENIO: apusys: add security ctrl setting
    add security ctrl setting
    
    Change-Id: Ifea13635a92a2ea5ec9ff709b22dad89b76de65d
    Signed-off-by: Chungying Lu <chungying.lu@mediatek.com>

---
+++ plat/mediatek/drivers/apusys/security_ctrl/1.0/rules.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2025, MediaTek Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+PLAT_INCLUDES += -I${MTK_PLAT}/drivers/apusys/security_ctrl/$(MTK_APUSYS_SEC_CTRL_VERSION)
+PLAT_INCLUDES += -I${MTK_PLAT}/drivers/apusys/${MTK_SOC}
+
